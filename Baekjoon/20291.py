2074ca83a7dcdcc24784d5cc645391dcc4345bfd
AngelPlayer <ssh5212@naver.com>
2022-05-15 19:07:07 +0900

    commit

--- Baekjoon/20291.py
+++ Baekjoon/20291.py
@@ -8,14 +8,14 @@ dic_extension = {}
 
 # process
 for i in range(T):
-    _, data = input().split('.')
+    _, data = input().split('.') # 확장자만 데이터 변수에 저장 
 
-    if data not in dic_extension:
-        dic_extension[data] = 1
-    else:
-        dic_extension[data] += 1
+    if data not in dic_extension: # 데이터가 기존에 존재 하지 않는다면
+        dic_extension[data] = 1 # 생성
+    else: # 존재한다면
+        dic_extension[data] += 1 # 증가
 
-dic_extension = sorted(dic_extension.items())
+dic_extension = sorted(dic_extension.items()) # 정렬
 
 # output
 for ext, count in dic_extension:
