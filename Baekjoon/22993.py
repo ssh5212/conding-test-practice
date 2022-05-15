cdf67827a96bcfa7c548805b50ccf4348934a8a7
AngelPlayer <ssh5212@naver.com>
2022-05-15 19:10:45 +0900

    commit

--- Baekjoon/22993.py
+++ Baekjoon/22993.py
@@ -8,15 +8,15 @@ T = int(input())
 
 # process
 damage_list = list(map(int, input().split()))
-my_damage = damage_list[0]
-damage_list.remove(damage_list[0]) 
-damage_list = sorted(damage_list)
+my_damage = damage_list[0] # 첫번째 유저
+damage_list.remove(damage_list[0]) # 첫 번째 요소(첫번째 유저) 제거
+damage_list = sorted(damage_list) # 리스트 정렬
 
 for i in range(T):
     if i != T-1:
-        if my_damage > damage_list[i]:
-            my_damage += damage_list[i]
-        else:
+        if my_damage > damage_list[i]: # 내 데미지가 큰 경우
+            my_damage += damage_list[i] 
+        else: # 내 데미지가 작은 경우
             print("No")
             break
     else:
