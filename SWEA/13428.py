930fd5df987e7a1173fba6bd19821fb9cd22a7f4
AngelPlayer <ssh5212@naver.com>
2022-05-16 18:44:54 +0900

    commit

--- SWEA/13428.py
+++ SWEA/13428.py
@@ -5,17 +5,39 @@ sys.stdin = open("13428_input.txt", "r")
 T = int(input())
 for test_case in range(1, T + 1):
     N = input() # 숫자 양수
-    max = int(N)
-    min = int(N)
 
-    for i in range(len(N)):
-        test = int(N[i]+N[0:i]+N[i+1:])
-        if max < test:
-            max = test
-        elif int(N[i]) != 0 and min > test:
-            min = test
+    num_list = list(map(str, N))
+    while '0' in num_list:
+        num_list.remove('0')
+    num_list.sort(reverse=True)
 
-    print(f'#{test_case} {min} {max}')
+    max_swap = 0
+    min_swap = 0
+    max_idx = 0
+    min_idx = 0
+
+    for i in range(len(num_list)):
+        if N[i] != num_list[i]:
+            max_idx = N.index(num_list[i])
+            max_swap = i
+            break
+    
+    for i in range(len(num_list)):
+        if N[i] != num_list[len(num_list)-i-1]:
+            min_idx = N.index(num_list[len(num_list)-i-1])
+            min_swap = i
+            break
+
+    max_num = N[:max_swap] + N[max_idx] + N[max_swap+1:max_idx] + N[max_swap] + N[max_idx+1:]
+
+    min_num = N[:min_swap] + N[min_idx] + N[min_swap+1:min_idx] + N[min_swap] + N[min_idx+1:]
+    
+    if max_swap == max_idx:
+        max_num = N
+    if min_swap == min_idx:
+        min_num = N
+
+    print(f'#{test_case} {min_num} {max_num}')
 
 
 
--- SWEA/test.py
+++ SWEA/test.py
@@ -1,12 +1,4 @@
-# N = '1234'
+N = [1,2,3,4]
 
-# for i in range(len(N)):
-#     print(N[i]+N[0:i]+N[i+1:])
-
-
-N = '1234'
-
-for i in range(len(N)):
-    for j in range(len(N)):
-        N[i], N[j] = N[j], N[i]
-        print(N)
+N.remove(1)
+print(N)
